14c5f293f1facc5f733a32cc4c7ca749f8af9d32
Kledson Renan <20863940+kledsonrenan@users.noreply.github.com>
2023-09-15 12:22:31 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let seconds = 5000; // 5 seconds
-
-setTimeout(() => {
-    $(() => {
-        let timeout = 3000;
-        let x = $('.navbar').offset().top;
-        $('html,body').animate({scrollTop:x}, timeout, 'swing');
-    });
-}, seconds);
